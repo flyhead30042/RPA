ae773fcdb263d194f2323e96ccdd5fd98866fe8f
flyhead30042 <cicicici.hsiao@gmail.com>
2020-11-09 16:57:53 +0800

    Init

---
+++ checkinout_ci.bat
@@ -0,0 +1,5 @@
+cd d:\workspace\citest\RPA
+conda activate RPA
+robot -v ID:c.i.hsiao@ericsson.com  -v PWD:brU4epC8    -t "Check In" checkinout.robot
+robot -v ID:c.i.hsiao@ericsson.com  -v PWD:brU4epC8    -t "Check Out" checkinout.robot
+
