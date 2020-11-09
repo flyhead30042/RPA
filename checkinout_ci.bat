202501c56a41a1c390bb58e95239f70658b454a3
flyhead30042 <cicicici.hsiao@gmail.com>
2020-11-09 21:25:02 +0800

    Rewrite

--- checkinout_ci.bat
+++ checkinout_ci.bat
@@ -1,5 +1,20 @@
-cd d:\workspace\citest\RPA
-conda activate RPA
-robot -v ID:c.i.hsiao@ericsson.com  -v PWD:brU4epC8    -t "Check In" checkinout.robot
-robot -v ID:c.i.hsiao@ericsson.com  -v PWD:brU4epC8    -t "Check Out" checkinout.robot
+@ECHO OFF
+@REM This batch file is to check in&out automatically
+
+ECHO =========================================
+ECHO
+ECHO Auto Check in/out script
+ECHO
+ECHO =========================================
+
+cls
+d:
+cd "d:\workspace\citest\RPA"
+ECHO "Change to %CD%"
+
+ECHO "Initiate Anaconda"
+CALL C:\Anaconda3\Scripts\activate.bat C:\Anaconda3
+CALL conda activate RPA
+robot.exe -v ID:c.i.hsiao@ericsson.com  -v PWD:brU4epC8  ./checkinout.robot
+
 
