9f8d5d7456ae906129ec3db4d88ef9e8b0f08aea
flyhead <cicicici.hsiao@gmail.com>
2021-08-02 23:43:45 +0800

    Commit

--- wtms/hello.py
+++ wtms/hello.py
@@ -1,6 +1,9 @@
 import logging
 import platform
+from time import sleep
+
 from selenium import webdriver
+
 
 logger = logging.getLogger(__name__)
 
@@ -13,22 +16,27 @@ def main():
     logger.info("Load webdriver")
     # driver = webdriver.Chrome(
     #     executable_path="webdriver/chromedriver-92.exe") if platform.system() == "Windows" else webdriver.Chrome()
-
+    PROXY = "www-proxy.apac.mgmt.ericsson.se:8080"
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    options.add_argument("--no-sandbox")   #Bypass OS security model
+    options.add_argument("--no-sandbox")   # bypass OS security model
     options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
-    # options.add_argument('--window-size=1420,1080')
-    options.add_argument('--disable-gpu')
+    # options.add_argument('--proxy-server=http://' + PROXY)
+
+    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+    capabilities["proxy"] = {
+        "httpProxy": PROXY,
+        "ftpProxy": PROXY,
+        "sslProxy": PROXY,
+        "proxyType": "MANUAL",
+    }
 
     # driver = webdriver.Remote("http://chrome:4444/wd/hub", options=options)
-    driver = webdriver.Remote("http://localhost:4444/wd/hub", options=options)
+    driver = webdriver.Remote("http://localhost:4444/wd/hub", options=options, desired_capabilities = capabilities)
 
     try:
-        logger.info("Open https://working-time-management-system-tw.internal.ericsson.com")
-        driver.get("https://working-time-management-system-tw.internal.ericsson.com")
-        # driver.get("https://www.google.com")
-        driver.
+        # driver.get("https://www.google.com/")
+        driver.get("https://working-time-management-system-tw.internal.ericsson.com/#/login")
         driver.save_screenshot('test.png')
         logging.info("title=" + driver.title)
     finally:
