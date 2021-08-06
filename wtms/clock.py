e0bddec13f17b65339510462f8a0c3fa31f03efc
flyhead <cicicici.hsiao@gmail.com>
2021-08-06 15:24:59 +0800

    Commit

--- wtms/clock.py
+++ wtms/clock.py
@@ -6,69 +6,73 @@ from selenium.common.exceptions import TimeoutException
 from browser import xpath, loginWithCredentials, Credential, wait_until_presence, WaitCondition, input_text, \
     click_element, TIMESTAMP1
 from selenium import webdriver
-import platform
 
-# Declare a global driver due to decoration
-driver = None
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument("--no-sandbox")  # Bypass OS security model
+options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
+options.add_argument('--ignore-ssl-errors=yes')  # bypass non-secure page
+options.add_argument('--ignore-certificate-errors')  # bypass non-secure page
+options.add_argument('--start-maximized')
+options.add_argument('--disable-gpu')
+driver = webdriver.Remote("http://chrome:4444/wd/hub", options=options)
 
 logger = logging.getLogger(__name__)
+level = logging.getLevelName(os.environ.get('LOG_LEVEL', 'INFO'))
+logger.setLevel(level)
 
 @wait_until_presence(driver = driver, wc=WaitCondition(timeout=15, xpath=xpath["LOGIN_SUBMIT_BTN"]))
 def login(driver, id:str, pwd:str):
-    loginWithCredentials(driver, Credential(id=id, pwd=pwd))
+    input_text(driver, xpath["LOGIN_ID_TF"], id)
+    input_text(driver, xpath["LOGIN_PWD_TF"], pwd)
+    driver.save_screenshot('screenshot/login.png')
+    click_element(driver, xpath["LOGIN_SUBMIT_BTN"])
 
 @wait_until_presence(driver=driver, wc=WaitCondition(timeout=15, xpath=xpath["CLOCK_TIME_TF"]))
 def clock_on(driver, time:str ="09:30"):
     input_text(driver, xpath["CLOCK_TIME_TF"], time)
+    driver.save_screenshot('screenshot/clock_on.png')
     click_element(driver, xpath["CLOCK_ON_DIV"])
 
 @wait_until_presence(driver=driver, wc=WaitCondition(timeout=15, xpath=xpath["CLOCK_TIME_TF"]))
 def clock_out(driver, time:str ="18:30"):
     input_text(driver, xpath["CLOCK_TIME_TF"], time)
+    driver.save_screenshot('screenshot/clock_out.png')
     click_element(driver, xpath["CLOCK_OUT_DIV"])
 
-
 def main():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument("--no-sandbox")   #Bypass OS security model
-    options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
-    options.add_argument('--window-size=1420,1080')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Remote("http://chrome:4444/wd/hub", options=options)
-
     try:
-        WTMS_URL = os.environ.get("WTMS_URL", "https://working-time-management-system-tw.internal.ericsson.com/#/login")
+        WTMS_URL = os.environ.get("WTMSURL", "https://working-time-management-system-tw.internal.ericsson.com/#/login")
         logger.info(f"1. Open {WTMS_URL}")
         driver.get(WTMS_URL)
-        driver.maximize_window()
+        # driver.maximize_window()
+        driver.save_screenshot('screenshot/open.png')
 
         logger.info("2. login with credentials")
         login(driver,
-              id=os.environ.get("WTMS_ID", "ecihsia"),
-              pwd=os.environ.get("WTMS_PWD", "ZAQ!xsw2"))
+              id=os.environ.get("WTMS_ID", "myid"),
+              pwd=os.environ.get("WTMS_PWD", "mypwd"))
 
         CLOCK_ON_TIME = os.environ.get("CLOCK_ON_TIME", "09:30")
         logger.info(F"3. Clock On at {CLOCK_ON_TIME}")
         clock_on(driver, time=CLOCK_ON_TIME )
-        sleep(1)
+        sleep(3)
 
         CLOCK_OUT_TIME = os.environ.get("CLOCK_OUT_TIME", "18:30")
         logger.info(f"4. Clock Out at {CLOCK_OUT_TIME}")
         clock_out(driver, time= CLOCK_OUT_TIME)
 
-        driver.save_screenshot('wtms_result.png')
-
     except TimeoutException as e1:
-        logger.error("TimeoutException:{}".format(str(e1)))
-        driver.get_screenshot_as_file("screenshot-{}.png".format(TIMESTAMP1))
+        logger.exception("TimeoutException:{}".format(str(e1)))
+        driver.save_screenshot("screenshot/TimeoutException-{}.png".format(TIMESTAMP1))
+        driver.quit()
     except ConnectionResetError as e2:
-        logger.error("ConnectionResetError:{}".format(str(e2)))
-        driver.get_screenshot_as_file("screenshot-{}.png".format(TIMESTAMP1))
+        logger.exception("ConnectionResetError:{}".format(str(e2)))
+        driver.save_screenshot("screenshot/ConnectionResetError-{}.png".format(TIMESTAMP1))
+        driver.quit()
     except Exception as e:
-        logger.error("Exception:{}".format(str(e)))
-        driver.get_screenshot_as_file("screenshot-{}.png".format(TIMESTAMP1))
-    finally:
+        logger.exception("Exception:{}".format(str(e)))
+        driver.save_screenshot("screenshot/Exception-{}.png".format(TIMESTAMP1))
         driver.quit()
 
 if __name__ == "__main__":
--- wtms/hello.py
+++ wtms/hello.py
@@ -1,10 +1,9 @@
 import logging
-import platform
-from time import sleep
-
+from  selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 
-
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s | %(name)s | %(levelname)s | %(message)s"
@@ -20,25 +19,19 @@ def main():
     options.add_argument('--headless')
     options.add_argument("--no-sandbox")   # bypass OS security model
     options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
-    # options.add_argument('--proxy-server=http://' + PROXY)
-
-    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-    capabilities["proxy"] = {
-        "httpProxy": PROXY,
-        "ftpProxy": PROXY,
-        "sslProxy": PROXY,
-        "proxyType": "MANUAL",
-    }
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
 
     # driver = webdriver.Remote("http://chrome:4444/wd/hub", options=options)
-    # driver = webdriver.Remote("http://localhost:4444/wd/hub", options=options, desired_capabilities = capabilities)
     driver = webdriver.Remote("http://localhost:4444/wd/hub", options=options)
 
     try:
-        # driver.get("https://www.google.com/")
+        # driver.get("http://cpistore.internal.ericsson.com/elex#")
         driver.get("https://working-time-management-system-tw.internal.ericsson.com/#/login")
-        driver.save_screenshot('test.png')
+
         logging.info("title=" + driver.title)
+        logging.info("page source=" + driver.page_source[:500])
+        driver.save_screenshot('test.png')
     finally:
         driver.quit()
 
