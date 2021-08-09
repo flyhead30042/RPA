4fbed526bcb2ee9746a89968dc5037d71d272f29
flyhead <cicicici.hsiao@gmail.com>
2021-08-09 19:35:44 +0800

    Update app.py

--- wtms/app.py
+++ wtms/app.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from typing import AnyStr
 
+from apscheduler.job import Job
 from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 
@@ -16,22 +17,14 @@ level = logging.getLevelName(os.environ.get('LOG_LEVEL', 'INFO'))
 logger.setLevel(level)
 os.environ['TZ'] = 'Asia/Taipei'
 
-# def get_cron_time(timestr:AnyStr):
-#     time = datetime.strptime(timestr, "%H:%M")
-#     cron_hr = str(time.hour)
-#     cron_min = str(time.minute)
-#     return cron_hr, cron_min
-
 if __name__ == "__main__":
     # BlockingScheduler: use when the scheduler is the only thing running in your process
     scheduler = BlockingScheduler()
 
     # Schedules the clock.main() to be executed.
-    # cron_hr, cron_min = get_cron_time(os.environ.get("CRON_CLOCK", "14:30"))
-
     crontab = os.environ.get("CRONTAB_CLOCK", "05 13 * * 1-5")
-    logger.info(f"0. Add clock job w/ schedule at {crontab}")
-    scheduler.add_job(clock.main, CronTrigger.from_crontab(crontab))
+    job:Job = scheduler.add_job(clock.main, CronTrigger.from_crontab(crontab), timezone='Asia/Taipei')
+    logger.info("0. Add {}(), {}".format(job.func_ref, job.trigger))
     # scheduler.add_job(clock.main, 'cron', day_of_week='mon-fri', hour=cron_hr, minute=cron_min,
     #                   start_date='2021-03-23 12:00:00', timezone='Asia/Taipei')
 
